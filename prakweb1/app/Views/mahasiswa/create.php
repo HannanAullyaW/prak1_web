e923273d4c31aa251db288c1dd71f79a40358680
2017051079_AullyaHananWulandari <aullya.hananwulandari2079@students.unila.ac.id>
2022-09-13 11:54:07 +0700

    update prak 3

---
+++ prakweb1/app/Views/mahasiswa/create.php
@@ -0,0 +1,15 @@
+<form>
+  <div class="form-group">
+    <label for="text">NPM</label>
+    <input type="npm" class="form-control" id="npm">
+  </div>
+  <div class="form-group">
+    <label for="text">Nama</label>
+    <input type="nama" class="form-control" id="nama">
+  </div>
+  <div class="form-group">
+    <label for="text">Alamat</label>
+    <input type="alamat" class="form-control" id="alamat">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
